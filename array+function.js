d5c383bd675ea9b662247815b8da21664e8c6bd0
Abuhenamosaddir <abuhenamosaddir8799@gmail.com>
2025-08-06 16:43:11 +0600

    add class 2

--- array+function.js
+++ array+function.js
@@ -1,12 +1,29 @@
-const fruits = [ 'apple', 'nanna', 'kanna', 'pamna']
+const fruits = [ 'apple', 'nanna', 'kanna', 'pamna', 'rabba', 'nnnn']
 //console.log(fruits[2]);
  
-    let length = fruits.length;
+    // let length = fruits.length;
 
-    console.log(length);
+    // console.log(length);
 
-for (let j = 1; j < length ; j++) {
+for (let j = 0; j < fruits.length ; j++) {
     console.log(j);
     console.log(fruits[j])
  }
- 
+ const numbers = [345, 45,67,55,3,56,334,45,5]
+ for (let i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+ }
+
+ let k = 0;
+ while ( k < fruits.length){
+    console.log(fruits[k]);
+    k++
+ }
+
+
+ const prics =[ 23, 44,65,3344,34,344]
+ let n= 0;
+ while (n < prics.length){
+    console.log(prics[n]);
+    n++
+ }
