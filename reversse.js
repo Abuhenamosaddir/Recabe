fbd81e5ac7f1d64aef19c364f1161e1f10602e53
Abuhenamosaddir <abuhenamosaddir8799@gmail.com>
2025-08-06 17:51:46 +0600

    reverse add

---
+++ reversse.js
@@ -0,0 +1,27 @@
+ const numbers = [ 44, 55, 66, 77, 88]
+ 
+ //1st track
+ const reverse = [];
+//  for (let i =0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+//     reverse.unshift(numbers[i]);
+//  } 
+
+//2nd track
+//  for (let i = numbers.length-1; i>=0; i--){
+//     reverse.push(numbers[i]);
+//  }
+
+//3rd teack
+
+// for (const num of numbers){
+//     //console.log(num);
+//     reverse.unshift(num);
+// }
+
+//4th track
+
+
+ console.log(numbers);
+ numbers.reverse();
+ console.log(numbers);
