0eedea526926854cffc27fb3fa1fc800f63fa01b
Abuhenamosaddir <abuhenamosaddir8799@gmail.com>
2025-08-07 10:20:59 +0600

    18 add

---
+++ practics.js
@@ -0,0 +1,14 @@
+const pri = 244;
+let com_pri = pri;  //let use for number change 
+com_pri = 3444;
+// pri = 232434;
+ console.log(pri);
+ console.log(com_pri);
+ 
+
+
+const pru = [23,35,455,34]
+const com_pru = pru;
+com_pru[2] = 300;
+console.log(pru);
+console.log(com_pru);
